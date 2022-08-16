07763466d47ce17a6bd6d7138decbb50ccfb8606
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-16 10:10:12 -0400

    commit tree d2f86c71339c7c495a095f1f3313910951e144aa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+
+    if a == b && b == c
+      :equilateral
+    elsif 
+      a == b || b == c || c == a
+      :isosceles
+    else
+       :scalene
+    end
+  end
+
+  def side_greater_than_zero?
+      a.positive? && b.positive? && c.positive?
+  end
+
+  def validate_triangle_inequality?
+      a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+      raise TriangleError unless side_greater_than_zero? && validate_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
